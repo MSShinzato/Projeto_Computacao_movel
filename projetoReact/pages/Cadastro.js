fb9d1c9f31b6a80166c532e4ee14718a67c01560
Lucas <55299150+projeto27@users.noreply.github.com>
2019-11-09 12:23:31 -0300

    Add files via upload

--- projetoReact/pages/Cadastro.js
+++ projetoReact/pages/Cadastro.js
@@ -1,47 +1,106 @@
 import React, {Component} from 'react';
-import {View,Button,TextInput,Text} from 'react-native';
+import {View,Button,TextInput,Text,ScrollView,AsyncStorage} from 'react-native';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
-/*
+let  radio_props = [
+    {label:"Casa", value: 0},
+    {label:"Apto", value: 1}
+];
 
-    tipo: casa ou apto
-    alugado: sim ou não
-    condomínio: sim ou não
-    valor
-    quant. de quartos
-    descricao
-    proprietário
-    data que precisa pagar aluguel
+let radio_cond =[
+    {label:"sim", value: 0},
+    {label:"não", value: 1}
+];
 
- */
+class Imovel{
 
+        constructor(tipo, isAlugado, valor, numQuarto, end, cidade, desc, dataAluguel, proprietario, isCond){
+        this.tipo = tipo;
+        this.isAlugado = isAlugado;
+        this.valor = valor;
+        this.numQuarto = numQuarto;
+        this.end = end;
+        this.cidade = cidade;
+        this.desc = desc;
+        this.proprietario = proprietario;
+        this.dataAluguel = dataAluguel;
+        this.isCond = isCond;
+    }
+
+}
 
 class Cadastro extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state  = {imovel:[], texto:""};
+        this.cadastrar = this.cadastrar.bind(this);
+        this.voltar = this.voltar.bind(this);
+    }
+    cadastrar() {
+        this.setState({imovel:[...this.state.imovel,this.state.texto]});
+        //this.setState({textp:""});
+        alert("Imovel Cadastrado com sucesso!!");
+    }
+    voltar() {
+        let t = this.props.navigation.pop();
+    }
+    txtProp(texto){
+         this.setState({Iproprietario:texto});
+    }
+    txtValor(texto){
+        this.setState({valor:texto});
+    }
+    txtData(texto){
+        this.setState({dataAluguel:texto});
+    }
+    txtQuarto(texto){
+        this.setState({numQuarto:texto});
+    }
+    txtDesc(texto){
+        this.setState({desc:texto});
+    }
+    txtEnd(texto){
+        this.setState({end:texto});
+    }
+    txtCidade(texto){
+        this.setState({cidade:texto});
+    }
     render() {
         return (
-            <View>
-                <Text>Cadastro</Text>
-                <Text>Nome do proprietário</Text>
-                <TextInput/>
-                <Text>Tipo do imóvel</Text>
-                <TextInput/>
-                <Text>Valor</Text>
-                <TextInput/>
-                <Text>Quantidade de quartos</Text>
-                <TextInput/>
-                <Text>Descrição</Text>
-                <TextInput/>
-                <Text>Data de aluguel</Text>
-                <TextInput/>
-                <Button title={"Salvar"} onPress={cadastrar()}></Button>
-                <Button title={"Voltar"} onPress={voltar()}></Button>
-            </View>
-        );
-        function cadastrar() {
+            <ScrollView>
+                <Text style={{fontSize:20}}>Cadastro</Text>
 
-        }
-        function voltar() {
-            let t = this.props.navigation.pop();
-        }
+                <Text>Nome do proprietário</Text>
+                <TextInput onChangeText={this.txtProp}/>
+                <Text>Tipo do imóvel</Text>
+                <RadioForm
+                    radio_props={radio_props}
+                    initial={0}
+                    onPress={(value) => {this.setState({value:value})}}
+                />
+                <Text>Condominio</Text>
+                <RadioForm
+                    radio_props={radio_cond}
+                    initial={0}
+                    onPress={(value) => {this.setState({value:value})}}
+                />
+                <Text>Valor</Text>
+                <TextInput onChangeText={this.txtValor}/>
+                <Text>Quantidade de quartos</Text>
+                <TextInput onChangeText={this.txtQuarto}/>
+                <Text>Endereço</Text>
+                <TextInput onChangeText={this.txtEnd}/>
+                <Text>Cidade</Text>
+                <TextInput onChangeText={this.txtCidade}/>
+                <Text>Descrição</Text>
+                <TextInput onChangeText={this.txtDesc}/>
+                <Text>Data de aluguel</Text>
+                <TextInput onChangeText={this.txtData}/>
+                <Button title={"Salvar"} onPress={this.cadastrar}></Button>
+                <Button title={"Voltar"} onPress={this.voltar}></Button>
+            </ScrollView>
+        );
     }
 }
 
--- projetoReact/pages/Home.js
+++ projetoReact/pages/Home.js
@@ -1,26 +1,54 @@
 import React, {Component} from 'react';
 import {Button, Text, View} from 'react-native';
+import {Image} from 'react-native';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.cadastrar = this.cadastrar.bind(this);
+        this.remover = this.remover.bind(this);
+        this.listar = this.listar.bind(this);
+        this.Integrantes = this.Integrantes.bind(this);
+    }
+
+    cadastrar() {
+        this.props.navigation.push("cadastroP");
+    }
+
+    remover() {
+        this.props.navigation.push("remocaoP");
+    }
+
+    listar() {
+        this.props.navigation.push("listaP");
+    }
+
+    Integrantes() {
+        this.props.navigation.push("IntegrantesP");
+    }
+
     render() {
         return (
             <View>
-                <Text>Imobiliária Lucas Souza</Text>
-                <Button title={"Cadastro"} onPress={cadastrar()}></Button>
-                <Button title={"Remoção"} onPress={remover()}></Button>
-                <Button title={"Lista de imovéis"} onPress={listar()}></Button>
+            <View style={{alignItems: 'center'}}>
+                <Text style={{fontSize: 20}}>Imobiliária Lucas Souza</Text>
+            </View>
+                <Image
+                    style={{width: 450, height: 300}}
+                    source={{uri: 'https://rankim.com.br/blog/wp-content/uploads/2016/07/Plataforma-Imobiliaria.jpg'}}
+                />
+                <View style={{margin: 25 }}>
+                    <Button title={"Cadastro"} onPress={this.cadastrar}></Button>
+                    <Button title={"Remoção"} onPress={this.remover}></Button>
+                    <Button title={"Lista de imovéis"} onPress={this.listar}></Button>
+                    <Button title={"Integrantes"} onPress={this.Integrantes}></Button>
+
+            </View>
             </View>
         );
-        function cadastrar() {
-            let push = this.props.navigation.push("cadastroP");
-        }
-        function remover() {
-            let push = this.props.navigation.push("remocaoP");
-        }
-        function listar() {
-            let push = this.props.navigation.push("listaP");
-        }
     }
+
+
 }
 
 export default Home;
